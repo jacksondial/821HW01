6e175e9faec2260a170e9375b39a30992875d961
jacksondial <61251187+jacksondial@users.noreply.github.com>
2022-01-17 11:48:58 -0500

    Add analyze_data function

--- Homework1.py
+++ Homework1.py
@@ -1,4 +1,6 @@
 # Homework 1
+import math as ma
+
 
 def get_data(string):
     """Take char string of numbers and convert to integer list of lists."""
@@ -9,5 +11,19 @@ def get_data(string):
     return ints
 
 
-str1 = "2 1 0 9 3 2"
-print(get_data(str1))
+str1 = "2 1 3"
+data1 = get_data(str1)
+
+
+def analyze_data(data, statistic):
+    """Take list of lists and outputs statistic about data."""
+    average = sum(data) // len(data)
+    if statistic == "average":
+        output = average
+    if statistic == "standard deviation":
+        sd = sum([(i - average) ** 2 for i in data]) // len(data)
+        output = sd
+    return output
+
+
+print(analyze_data(data1, "standard deviation"))
