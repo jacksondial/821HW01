e5a366991e349a10dba122c1c8cbcdeddbca690b
bjsucher <brandonsucher1@gmail.com>
2022-01-18 09:36:47 -0500

    Edited average and sd to work for multiple lists of ints

--- Homework1.py
+++ Homework1.py
@@ -2,7 +2,6 @@
 import math as ma
 
 
-
 def get_data(string):
     """Take char string of numbers and convert to integer list of lists."""
     new_list = list(string.split("\n"))
@@ -21,25 +20,25 @@ def get_data(string):
     return final_list
 
 
-<<<<<<< HEAD
-str1 = "2 1 3"
+str1 = """1 44 31 4 5 6
+21 3 2 13 55 72"""
 data1 = get_data(str1)
 
 
 def analyze_data(data, statistic):
     """Take list of lists and outputs statistic about data."""
-    average = sum(data) // len(data)
+    all_data = [number for lst in data for number in lst]
+    average = sum(all_data) / len(all_data)
     if statistic == "average":
-        output = average
+        output = round(average, 1)
     if statistic == "standard deviation":
-        sd = sum([(i - average) ** 2 for i in data]) // len(data)
-        output = sd
+        sd = ma.sqrt(sum([(i - average) ** 2 for i in all_data]) / len(all_data))
+        output = round(sd, 1)
     return output
 
 
+print(analyze_data(data1, "average"))
 print(analyze_data(data1, "standard deviation"))
-=======
-str1 = """2 1 0 9 3 2
-4 7 6 1 2 5"""
-print(get_data(str1))
->>>>>>> 68a2defb7b166b863fd43d49123de2e5cc96fbcf
+# str1 = """2 1 0 9 3 2
+# 4 7 6 1 2 5"""
+# print(get_data(str1))
